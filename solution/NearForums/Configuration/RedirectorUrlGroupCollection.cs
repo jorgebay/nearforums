e2a5b7ba7a981674f164bbeeb4e8a75a061bd66c
jorgebg <none@none>
2010-06-28 08:27:54 +0000

    Implemented Redirector module (from http://www.codeproject.com/KB/aspnet/redirector.aspx). MySql provider included.
    --HG--
    extra : convert_revision : svn%3A755fb503-1302-0b44-8f45-eece28f159d8/trunk%4048

---
+++ solution/NearForums/Configuration/RedirectorUrlGroupCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+
+namespace NearForums.Configuration
+{
+	public class RedirectorUrlGroupCollection : ConfigurationElementCollection
+	{
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new RedirectorUrlGroup();
+		}
+
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			return ((RedirectorUrlGroup)(element)).Regex;
+		}
+
+		public RedirectorUrlGroup this[int idx]
+		{
+			get
+			{
+				return (RedirectorUrlGroup)BaseGet(idx);
+			}
+		}
+	}
+}
