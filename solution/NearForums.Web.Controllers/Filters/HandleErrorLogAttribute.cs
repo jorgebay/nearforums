24beceb54f65a537ef067420502e6374b05b3b48
jorgebg <jorgebaygondra at gmail>
2011-02-17 15:46:56 +0100

    Cleaned HandleErrorLogAttribute

--- solution/NearForums.Web.Controllers/Filters/HandleErrorLogAttribute.cs
+++ solution/NearForums.Web.Controllers/Filters/HandleErrorLogAttribute.cs
@@ -18,31 +18,5 @@ namespace NearForums.Web.Controllers.Filters
 			}
 			base.OnException(filterContext);
 		}
-
-		///// <summary>
-		///// Based on with type of exception and stacktrace, 
-		///// it determines if the exception should be logged as a warning
-		///// </summary>
-		//protected virtual bool IsWarning(Exception ex)
-		//{
-		//    bool isWarning = false;
-
-		//    switch (ex.GetType().Name)
-		//    {
-		//        case "ArgumentException":
-		//            if (ex.StackTrace.Contains("Mvc.ReflectedActionDescriptor.ExtractParameterFromDictionary"))
-		//            {
-		//                //The parameters of the action are incorrect (based on the constrains and parameter definition.
-		//                isWarning = true;
-		//            }
-		//            break;
-		//        case "HttpRequestValidationException":
-		//            //A potentially dangerous request.form posted.
-		//            isWarning = true;
-		//            break;
-		//    }
-
-		//    return isWarning;
-		//} 
 	}
 }
