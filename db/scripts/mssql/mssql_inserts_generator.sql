1c652cbee0bd61ae4887cd54ff405ba1ace0d681
jorgebg <jorgebaygondra at gmail>
2011-02-28 14:57:46 +0100

    mssql scripts: added "if not exist" and adapted insert sql file
    --HG--
    branch : version-005

--- db/scripts/mssql/mssql_inserts_generator.sql
+++ db/scripts/mssql/mssql_inserts_generator.sql
@@ -1,5 +1,17 @@
-select '/*1*/INSERT INTO dbo.UsersGroups (UserGroupId, UserGroupName) VALUES (' + CONVERT(varchar,UserGroupId) + ', ''' + UserGroupName + ''')' from usersgroups
+select '/*030*/ IF (SELECT COUNT(*) FROM dbo.UsersGroups) = 0'
 UNION
-SELECT '/*2*/GO'
+select '/*040*/ BEGIN'
 UNION
-SELECT '/*3*/INSERT INTO dbo.ForumsCategories (CategoryName, CategoryOrder) VALUES (''General'', 1)'
+select '/*050*/ INSERT INTO dbo.UsersGroups (UserGroupId, UserGroupName) VALUES (' + CONVERT(varchar,UserGroupId) + ', ''' + UserGroupName + ''')' from usersgroups
+UNION
+select '/*055*/ END'
+UNION
+SELECT '/*060*/ GO'
+UNION
+select '/*070*/ IF (SELECT COUNT(*) FROM dbo.ForumsCategories) = 0'
+UNION
+select '/*080*/ BEGIN'
+UNION
+SELECT '/*090*/ INSERT INTO dbo.ForumsCategories (CategoryName, CategoryOrder) VALUES (''General'', 1)'
+UNION
+SELECT '/*100*/ END'
