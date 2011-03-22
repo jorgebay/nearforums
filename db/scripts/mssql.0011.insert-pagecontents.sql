185b88553118226112af93d2ce2408cb12601d7d
jorgebg <jorgebaygondra at gmail>
2011-03-22 13:03:21 +0100

    PageContent: Added about page db script. Removed static about page

---
+++ db/scripts/mssql.0011.insert-pagecontents.sql
@@ -0,0 +1,5 @@
+﻿DELETE FROM dbo.[PageContents]
+INSERT INTO dbo.[PageContents] (PageContentTitle, PageContentShortName, PageContentEditDate, PageContentBody)
+	VALUES ('About', 'about', GETUTCDATE(), '
+	<p>This forum is powered by <a href="http://www.nearforums.com">Nearforums</a>, an open source forum engine.</p>
+	<p>Nearforums is released under <a href="http://nearforums.codeplex.com/license" target="_blank">MIT License</a>, you can get the source at <a href="http://www.nearforums.com/source-code">www.nearforums.com/source-code</a>.</p>')
