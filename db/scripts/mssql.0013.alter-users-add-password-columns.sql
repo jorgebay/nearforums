6d0f67e50763a7c98e0c05c2749495d7452a6f20
jorgebg <jorgebaygondra at gmail>
2011-04-04 10:46:40 +0200

    sql script: Add columns PasswordResetGuid and PasswordResetGuidExpireDate to table Users
    --HG--
    rename : db/scripts/mssql.0013.create-passwordreset-objects.sql => db/scripts/mssql.0014.create-passwordreset-objects.sql

---
+++ db/scripts/mssql.0013.alter-users-add-password-columns.sql
@@ -0,0 +1,16 @@
+﻿BEGIN TRY
+    ALTER TABLE	dbo.[Users] DROP COLUMN PasswordResetGuid;
+END TRY
+BEGIN CATCH
+    -- Execute error retrieval routine.
+END CATCH;
+BEGIN TRY
+    ALTER TABLE	dbo.[Users] DROP COLUMN PasswordResetGuidExpireDate;
+END TRY
+BEGIN CATCH
+    -- Execute error retrieval routine.
+END CATCH;
+GO
+ALTER TABLE	dbo.[Users] ADD PasswordResetGuid varchar(100) null
+GO
+ALTER TABLE	dbo.[Users] ADD PasswordResetGuidExpireDate datetime null
