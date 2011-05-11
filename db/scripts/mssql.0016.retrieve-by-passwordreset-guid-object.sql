a35a69c8a323ba8eef6a2f077e7e5f44e6e4b4e0
jasonco <none@none>
2011-05-11 08:55:12 -0400

    Db Object password reset: retrieve user by password reset guid

---
+++ db/scripts/mssql.0016.retrieve-by-passwordreset-guid-object.sql
@@ -0,0 +1,31 @@
+﻿IF EXISTS(SELECT * FROM [dbo].[sysobjects]
+	WHERE ID = object_id(N'[dbo].[SPUsersGetByPasswordResetGuid]') 
+	AND xtype in ('U', 'P', 'IF', 'V'))
+	DROP PROCEDURE SPUsersGetByPasswordResetGuid;
+GO
+
+
+CREATE PROCEDURE [dbo].[SPUsersGetByPasswordResetGuid]
+	@Provider varchar(32)
+	,@PasswordResetGuid varchar(64)
+AS
+SELECT 
+	U.UserId
+	,U.UserName
+	,U.UserGroupId
+	,U.UserGuid
+	,U.UserTimeZone
+	,U.UserExternalProfileUrl
+	,U.UserProviderLastCall
+	,U.UserEmail
+	,U.UserProviderId
+	,U.PasswordResetGuid
+	,U.PasswordResetGuidExpireDate
+FROM
+	Users U
+WHERE
+	UserProvider = @Provider
+	AND
+	PasswordResetGuid = @PasswordResetGuid
+
+GO
