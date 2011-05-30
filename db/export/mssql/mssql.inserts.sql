ba34b94877d7ec4cd5ac5f00c744ac41d703ab9f
jorgebg <jorgebaygondra at gmail>
2011-05-30 15:35:31 +0200

    Added go to the insert sql script
    --HG--
    branch : version-006

--- db/export/mssql/mssql.inserts.sql
+++ db/export/mssql/mssql.inserts.sql
@@ -1,4 +1,5 @@
-﻿IF (SELECT COUNT(*) FROM dbo.UsersGroups) = 0
+﻿GO
+IF (SELECT COUNT(*) FROM dbo.UsersGroups) = 0
 BEGIN
 	INSERT INTO dbo.UsersGroups (UserGroupId, UserGroupName) VALUES (1, 'Level 1')
 	INSERT INTO dbo.UsersGroups (UserGroupId, UserGroupName) VALUES (10, 'Admin')
@@ -21,4 +22,4 @@ BEGIN
 		<p>This forum is powered by <a href="http://www.nearforums.com">Nearforums</a>, an open source forum engine.</p>
 		<p>Nearforums is released under <a href="http://nearforums.codeplex.com/license" target="_blank">MIT License</a>, you can get the source at <a href="http://www.nearforums.com/source-code">www.nearforums.com/source-code</a>.</p>')
 END
-	
+GO
