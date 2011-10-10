17ab86f930896e830939aa02f542b452c4d4934a
jorgebg (jorgebaygondra at gmail) <none@none>
2011-10-10 16:40:09 +0200

    Fixed template key max length on add

---
+++ db/scripts/mssql.0026.alter-proc-templates-insert.sql
@@ -0,0 +1,26 @@
+﻿/****** Object:  StoredProcedure [dbo].[SPTemplatesInsert]    Script Date: 10/10/2011 16:29:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[SPTemplatesInsert]
+	@TemplateKey varchar(64)
+	,@TemplateDescription varchar(256)
+	,@TemplateId int OUTPUT
+AS
+INSERT INTO Templates
+(
+	TemplateKey
+	,TemplateDescription
+	,TemplateDate
+	,TemplateIsCurrent
+)
+VALUES
+(
+	@TemplateKey
+	,@TemplateDescription
+	,GETUTCDATE()
+	,0
+)
+
+SELECT @TemplateId = @@IDENTITY
