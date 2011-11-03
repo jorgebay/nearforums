1c1d0daffeb96da5e18bd6c5550cb493007cd187
jorgebg (jorgebaygondra at gmail) <none@none>
2011-11-03 15:00:57 +0100

    AssemblyVersion to 7.0

--- solution/SolutionInfo.cs
+++ solution/SolutionInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("NearForums.Tests")]
 
 //Only modify the assembly version values on pre release commits, only changes to major and minor
-[assembly: AssemblyVersion("6.0")]
+[assembly: AssemblyVersion("7.0")]
 //Major and minor are copied from assembly version. Real revisions.
-[assembly: AssemblyFileVersion("6.0.3.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
