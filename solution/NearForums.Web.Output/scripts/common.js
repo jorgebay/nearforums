30d36d30d9a1bfcd0d14a6f82aeca6a82d756e6b
jorgebg (jorgebaygondra at gmail) <none@none>
2011-11-24 11:55:09 +0100

    Localized Js messages

--- solution/NearForums.Web.Output/scripts/common.js
+++ solution/NearForums.Web.Output/scripts/common.js
@@ -1,24 +1,16 @@
-﻿function deleteMessage(mid, url, sender)
+﻿function deleteMessageCommon(mid, url, confirmMessage, removedMessage, sender)
 {
-    if (confirm('Are you sure you want to hide the message #' + mid + '?'))
+    if (confirm(confirmMessage.replace('{0}', mid)))
     {
         $.post(url, { mid: mid }, function (data)
         {
-            $("#msg" + mid + " > div.msgBody").html("<p class=\"removed\">Message removed</p>");
-            alert('The message was removed.');
+            $("#msg" + mid + " > div.msgBody").html("<p class=\"removed\">" + removedMessage + "</p>");
+            alert(removedMessage);
             if (sender)
             {
                 $(sender).closest("li").remove();
             }
         });
     }
-    return false;
-}
-function flagMessage(mid, url)
-{
-    $.post(url, { mid: mid }, function (data)
-    {
-        alert('Thanks you, a moderator will review the message.');
-    });
     return false;
 }
