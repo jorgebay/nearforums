e40cb4baaad17404f1180e9452221bf1d4c5e252
jorgebg (jorgebaygondra at gmail) <none@none>
2012-01-25 16:50:17 +0100

    Read and post access rights for Forums: Db objects

---
+++ db/scripts/mssql.0033.alter-table-forums-add-readwrite-access-columns.sql
@@ -0,0 +1,31 @@
+﻿BEGIN TRY
+    ALTER TABLE	dbo.Forums DROP CONSTRAINT FK_Forums_UsersGroups_Read;
+    ALTER TABLE	dbo.Forums DROP COLUMN ReadAccessGroupId;
+END TRY
+BEGIN CATCH
+    -- The column and fk does not exist: do nothing
+END CATCH;
+BEGIN TRY
+    ALTER TABLE	dbo.Forums DROP CONSTRAINT FK_Forums_UsersGroups_Post;
+    ALTER TABLE	dbo.Forums DROP COLUMN PostAccessGroupId;
+END TRY
+BEGIN CATCH
+    -- The column and fk does not exist: do nothing
+END CATCH;
+GO
+ALTER TABLE	dbo.Forums ADD ReadAccessGroupId SMALLINT NULL
+GO
+ALTER TABLE	dbo.Forums ADD PostAccessGroupId SMALLINT NULL
+GO
+
+ALTER TABLE dbo.Forums  WITH CHECK ADD  CONSTRAINT FK_Forums_UsersGroups_Read FOREIGN KEY(ReadAccessGroupId)
+	REFERENCES dbo.UsersGroups (UserGroupId)
+ALTER TABLE dbo.Forums  WITH CHECK ADD  CONSTRAINT FK_Forums_UsersGroups_Post FOREIGN KEY(PostAccessGroupId)
+	REFERENCES dbo.UsersGroups (UserGroupId)
+GO
+
+UPDATE Forums 
+	SET PostAccessGroupId = 1;
+GO
+
+ALTER TABLE	dbo.Forums ALTER COLUMN PostAccessGroupId SMALLINT NOT NULL;
---
+++ db/scripts/mssql.0034.alter-procs-forums-readwrite-access-columns.sql
@@ -0,0 +1,123 @@
+﻿GO
+ALTER PROCEDURE [dbo].[SPForumsGetByCategory]
+	@UserGroupId smallint=NULL
+AS
+SELECT
+	F.ForumId
+	,F.ForumName
+	,F.ForumShortName
+	,F.ForumDescription
+	,F.UserId
+	,F.ForumCreationDate
+	,F.ForumTopicCount
+	,F.ForumMessageCount
+	,C.CategoryId
+	,C.CategoryName
+FROM
+	ForumsCategories C
+	INNER JOIN Forums F ON F.CategoryId = C.CategoryId
+WHERE
+	F.Active = 1
+	AND
+	ISNULL(F.ReadAccessGroupId,-1) <= ISNULL(@UserGroupId,-1)
+ORDER BY
+	C.CategoryOrder,
+	F.ForumOrder
+GO
+
+ALTER PROCEDURE [dbo].[SPForumsGetByShortName]
+	@ShortName nvarchar(32)
+AS
+SELECT
+	F.ForumId
+	,F.ForumName
+	,F.ForumShortName
+	,F.ForumDescription
+	,F.UserId
+	,F.ForumCreationDate
+	,F.ForumTopicCount
+	,F.ForumMessageCount
+	,C.CategoryId
+	,C.CategoryName
+	,F.ReadAccessGroupId
+	,F.PostAccessGroupId
+FROM
+	Forums F 
+	INNER JOIN ForumsCategories C ON F.CategoryId = C.CategoryId
+WHERE
+	F.ForumShortName = @ShortName
+	AND
+	F.Active = 1
+GO
+
+ALTER PROCEDURE [dbo].[SPForumsInsert]
+(
+	@ForumName nvarchar(255)
+	,@ForumShortName nvarchar(32)
+	,@ForumDescription nvarchar(max)
+	,@CategoryId int
+	,@UserId int
+	,@ReadAccessGroupId smallint
+	,@PostAccessGroupId smallint
+)
+AS
+INSERT INTO Forums
+(
+	ForumName
+	,ForumShortName
+	,ForumDescription
+	,CategoryId
+	,UserId
+	,ForumCreationDate
+	,ForumLastEditDate
+	,ForumLastEditUser
+	,Active
+	,ForumTopicCount
+	,ForumMessageCount
+	,ForumOrder
+	,ReadAccessGroupId
+	,PostAccessGroupId
+)
+VALUES
+(
+	@ForumName
+	,@ForumShortName
+	,@ForumDescription
+	,@CategoryId
+	,@UserId
+	,GETUTCDATE()
+	,GETUTCDATE()
+	,@UserId
+	,1
+	,0
+	,0
+	,0
+	,@ReadAccessGroupId
+	,@PostAccessGroupId
+)
+GO
+
+ALTER PROCEDURE [dbo].[SPForumsUpdate]
+(
+	@ForumShortName nvarchar(32)
+	,@ForumName nvarchar(255)
+	,@ForumDescription nvarchar(max)
+	,@CategoryId int
+	,@UserId int
+	,@ReadAccessGroupId smallint
+	,@PostAccessGroupId smallint
+)
+AS
+UPDATE Forums
+SET
+	ForumName = @ForumName
+	,ForumDescription = @ForumDescription 
+	,CategoryId = @CategoryId
+	,ForumLastEditDate = GETUTCDATE()
+	,ForumLastEditUser = @UserId
+	,ReadAccessGroupId = @ReadAccessGroupId
+	,PostAccessGroupId = @PostAccessGroupId
+WHERE
+	ForumShortName = @ForumShortName
+GO
+
