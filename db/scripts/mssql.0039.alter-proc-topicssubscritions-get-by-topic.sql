069a8217cd747aa24375b4de18ed719025f8e329
jorgebg (jorgebaygondra at gmail) <none@none>
2012-01-31 12:58:08 +0100

    Read access rights for Topics: db proc for subscriptions

---
+++ db/scripts/mssql.0039.alter-proc-topicssubscritions-get-by-topic.sql
@@ -0,0 +1,23 @@
+﻿ALTER PROCEDURE [dbo].[SPTopicsSubscriptionsGetByTopic]
+	@TopicId int
+AS
+/*
+	Gets the active users subscribed to a topic.
+	Checks read access of topic vs user role
+*/
+SELECT
+	U.UserId
+	,U.UserName
+	,U.UserEmail
+	,U.UserEmailPolicy
+	,U.UserGuid
+FROM
+	TopicsSubscriptions S
+	INNER JOIN Topics T ON T.TopicId = S.TopicId
+	INNER JOIN Users U ON U.UserId = S.UserId
+WHERE
+	S.TopicId = @TopicId
+	AND
+	U.Active = 1
+	AND
+	U.UserGroupId >= ISNULL(T.ReadAccessGroupId, -1)
