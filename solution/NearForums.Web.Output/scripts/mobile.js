84a09a3148aeb8e6157029555f9a3bb005ef41d5
jorgebg (jorgebaygondra at gmail) <none@none>
2012-02-22 12:53:44 +0100

    Updated Mobile Layout

--- solution/NearForums.Web.Output/scripts/mobile.js
+++ solution/NearForums.Web.Output/scripts/mobile.js
@@ -1,8 +1,11 @@
 ﻿$("div").live("pagebeforecreate", function(){
 	//inserts jquery mobile attributes to behave like a mobile interface
+	$.mobile.page.prototype.options.addBackBtn= true;
 	$("div.toolbar, div.pager").attr("data-role", "controlgroup").attr("data-type", "horizontal");
 	$("div.toolbar a, div.pager a, div.pager span").attr("data-role", "button");
 	$("ul.items").attr("data-role", "listview");
-	//$("ul.items span.details").addClass("ui-li-count");
+	$("#messages").attr("data-role", "listview");
+	$("ul.items div.topicCount, ul.items div.views, ul.items div.replies").addClass("ui-li-count");
+	$("ul.items div.separator").hide();
 });
 $("#messages li").live("tap", function () { $(this).toggleClass("over"); });
