beafaadb7dcb3d4c8993843d39c041f1a8efbced
jorgebg (jorgebaygondra at gmail) <none@none>
2012-02-29 17:16:20 +0100

    SolutionInfo: Changed version to v8.0.0.0

--- solution/SolutionInfo.cs
+++ solution/SolutionInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("NearForums.Tests")]
 
 //Only modify the assembly version values on pre release commits, only changes to major and minor
-[assembly: AssemblyVersion("7.0")]
+[assembly: AssemblyVersion("8.0")]
 //Major and minor are copied from assembly version. Real revisions.
-[assembly: AssemblyFileVersion("7.0.3.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
