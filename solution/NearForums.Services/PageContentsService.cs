eed2fa205ad44d57834f800585e7b32dabcdc5c8
jorgebg (jorgebaygondra at gmail) <none@none>
2012-03-22 17:37:04 +0100

    Created PageContentsService and its interface
    --HG--
    branch : di-refac

---
+++ solution/NearForums.Services/PageContentsService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NearForums.DataAccess;
+
+namespace NearForums.Services
+{
+	public class PageContentsService : IPageContentsService
+	{
+		private readonly IPageContentsDataAccess dataAccess;
+
+		public PageContentsService(IPageContentsDataAccess da)
+		{
+			dataAccess = da;
+		}
+
+		public List<PageContent> GetAll()
+		{
+			return dataAccess.GetAll();
+		}
+
+		public PageContent Get(string name)
+		{
+			return dataAccess.Get(name);
+		}
+
+		public void Add(PageContent content)
+		{
+			content.ValidateFields();
+			SetAvailableShortName(content);
+			dataAccess.Add(content);
+		}
+
+		public void SetAvailableShortName(PageContent content)
+		{
+			content.ShortName = dataAccess.GetAvailableShortName(content.ShortName);
+		}
+
+		public void Edit(PageContent content)
+		{
+			content.ValidateFields();
+			dataAccess.Edit(content);
+		}
+
+		public bool Delete(string name)
+		{
+			return dataAccess.Delete(name);
+		}
+	}
+}
