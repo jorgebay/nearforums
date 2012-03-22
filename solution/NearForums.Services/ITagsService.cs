c6bce20a4a8210cda8fca717f420ac2e5d3c22f8
jorgebg (jorgebaygondra at gmail) <none@none>
2012-03-22 16:59:38 +0100

    TagsService and interface
    --HG--
    branch : di-refac

---
+++ solution/NearForums.Services/ITagsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NearForums.Services
+{
+	public interface ITagsService
+	{
+		List<WeightTag> GetMostViewed(int forumId, int top);
+	}
+}
---
+++ solution/NearForums.Services/TagsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NearForums.DataAccess;
+
+namespace NearForums.Services
+{
+	public class TagsService : ITagsService
+	{
+		private readonly ITagsDataAccess dataAccess;
+
+		public TagsService(ITagsDataAccess da)
+		{
+			dataAccess = da;
+		}
+
+		public List<WeightTag> GetMostViewed(int forumId, int top)
+		{
+			return dataAccess.GetMostViewed(forumId, top);
+		}
+	}
+}
