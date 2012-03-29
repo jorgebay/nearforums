8b3cb03f3a2f76da1d8824a3656058fe2a3e1820
jorgebg (jorgebaygondra at gmail) <none@none>
2012-03-29 15:56:21 +0200

    Created NotificationsService and its interface
    --HG--
    branch : di-refac

---
+++ solution/NearForums.Services/INotificationsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace NearForums.Services
+{
+	interface INotificationsService
+	{
+		void SendMail(MailMessage message);
+		/// <summary>
+		/// Sends the mail to reset the password to a user
+		/// </summary>
+		/// <param name="user"></param>
+		/// <param name="url"></param>
+		void SendResetPassword(User user, string url);
+		/// <summary>
+		/// Sync Sends a notification to every user subscribed to a topic
+		/// </summary>
+		/// <param name="topic"></param>
+		/// <param name="userId">userId of the last poster</param>
+		int SendToUsersSubscribed(Topic topic, List<User> users, string body, string url, string unsubscribeUrl, bool handleExceptions);
+	}
+}
