ad83d0ed435dc738a3575f69d1e4036797bfdbc9
jorgebg (jorgebaygondra at gmail) <none@none>
2012-05-25 16:54:33 +0200

    Deleted NearForums.ServiceClient.csproj and its files
    --HG--
    branch : di-refac

--- solution/NearForums.ServiceClient/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NearForums.ServiceClient")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("NearForums.ServiceClient")]
-[assembly: Guid("788745aa-81e6-48f0-9773-0aa7c164c412")]
