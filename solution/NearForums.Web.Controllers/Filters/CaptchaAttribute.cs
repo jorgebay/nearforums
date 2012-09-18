0a3683acdad8be60e9ad17ea1ac8449ba00accc9
jorgebg (jorgebaygondra at gmail) <none@none>
2012-09-18 15:26:58 +0200

    Added Captcha action filter

---
+++ solution/NearForums.Web.Controllers/Filters/CaptchaAttribute.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NearForums.Validation;
+using NearForums.Web.Extensions;
+using NearForums.Web.State;
+using System.Threading;
+
+namespace NearForums.Web.Controllers.Filters
+{
+	public class CaptchaAttribute : BaseActionFilterAttribute
+	{
+		private const string _captchaModelStateKey = "captcha";
+
+		private bool _showCaptcha = true;
+		public bool ShowCaptcha
+		{
+			get
+			{
+				return _showCaptcha;
+			}
+			set
+			{
+				_showCaptcha = value;
+			}
+		}
+
+		/// <summary>
+		/// Determines if the request is POST
+		/// </summary>
+		protected virtual bool IsPost(ControllerContext context)
+		{
+			return context.HttpContext.Request.HttpMethod == "POST";
+		}
+
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			if (IsPost(filterContext))
+			{
+				if (ValidateCaptchaModelState(filterContext))
+				{
+					ShowCaptcha = false;
+				}
+			}
+
+			filterContext.Controller.ViewBag.ShowCaptcha = ShowCaptcha;
+
+			base.OnActionExecuting(filterContext);
+		}
+
+		/// <summary>
+		/// Checks if captcha value is invalid. If so, it add a model error to the current ModelState
+		/// </summary>
+		protected virtual bool ValidateCaptchaModelState(ActionExecutingContext filterContext)
+		{
+			var isValid = false;
+			var captchaModelState = filterContext.Controller.ViewData.ModelState[_captchaModelStateKey];
+			if (captchaModelState == null)
+			{
+				captchaModelState = new ModelState();
+				var postedValue = filterContext.HttpContext.Request.Form[_captchaModelStateKey];
+				captchaModelState.Value = new ValueProviderResult(postedValue, postedValue, Thread.CurrentThread.CurrentUICulture);
+				filterContext.Controller.ViewData.ModelState.Add(_captchaModelStateKey, captchaModelState);
+			}
+
+			//if its the captcha value is not correct, add a modelState error
+			if (String.IsNullOrEmpty(captchaModelState.Value.AttemptedValue))
+			{
+				captchaModelState.Errors.Add(new ValidationError(_captchaModelStateKey, ValidationErrorType.NullOrEmpty));
+			}
+			else if (!CaptchaHelper.IsValidCaptchaValue(captchaModelState.Value.AttemptedValue, new SessionWrapper(filterContext.HttpContext)))
+			{
+				captchaModelState.Errors.Add(new ValidationError(_captchaModelStateKey, ValidationErrorType.CompareNotMatch));
+			}
+			else
+			{
+				isValid = true;
+			}
+
+			return isValid;
+		} 
+
+		/// <summary>
+		/// Called after the action method executes
+		/// </summary>
+		public override void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			base.OnActionExecuted(filterContext);
+		}
+	}
+}
