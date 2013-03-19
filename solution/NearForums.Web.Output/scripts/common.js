82a2fbd997609291a7603b472b6ee380cd5359d7
jorgebg (jorgebaygondra at gmail) <none@none>
2013-03-19 18:26:41 +0100

    User Detail Toolbar for moderation

--- solution/NearForums.Web.Output/scripts/common.js
+++ solution/NearForums.Web.Output/scripts/common.js
@@ -47,3 +47,19 @@ function postAndContinue(sender, data, confirmMessage)
 	}
 	return false;
 }
+$.fn.serializeObject = function () 
+{
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function () {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+};
